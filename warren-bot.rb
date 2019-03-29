dc7ab3fafbbdb364cf4f0d71933a6dc07d0dc9a1
Travis Rasor <travis@thathanka.org>
2019-03-29 14:01:08 -0500

    removing web components

--- warren-bot.rb
+++ warren-bot.rb
@@ -3,4 +3,10 @@ Dotenv.load unless (ENV['RACK_ENV'] && ENV['RACK_ENV'] == 'production')
 
 require_relative './bot/bot'
 
-Warren::Bot.run
+begin
+    Warren::Bot.run
+rescue Exception => e
+  STDERR.puts "warren-bot: ERROR! #{e}"
+  STDERR.puts e.backtrace
+end
+  
